f5bbdc27db0959f964d79a159236bc3206046fbd
Marcela Mejia <mejiao24@hotmail.com>
2023-09-19 21:16:47 +1000

    complete props planets challenge

---
+++ REACT/props-planets-challenge/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header/Header.jsx";
+import PlanetsList from "./container/PlanetsList.jsx";
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header>
+        <h1>Planets</h1>
+      </Header>
+      <PlanetsList />
+    </>
+  )
+}
+
+export default App;
